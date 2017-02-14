ff883379cf9e914e981c2dec9046fc56d62febac
Bryce Kerley <bkerley@brycekerley.net>
2017-02-13 20:01:31 -0500

    cat knows about human and bowl

--- lib/catherder/cat.rb
+++ lib/catherder/cat.rb
@@ -2,8 +2,14 @@ require 'catherder/sprite'
 
 module Catherder
   class Cat < Sprite
-    def initialize(window)
+    attr_accessor :human, :bowl
+
+    def initialize(window: , human: , bowl: )
       super window
+
+      self.human = human
+      self.bowl = bowl
+
       load_image "cat.png"
 
       self.x = rand(window.width / 2) + (window.width / 4)
--- lib/catherder/window.rb
+++ lib/catherder/window.rb
@@ -1,14 +1,18 @@
 module Catherder
   class Window < Gosu::Window
+    attr_accessor :human, :cat, :bowl
+
     def initialize
       super 800, 600, false
       self.caption = 'cat herder'
 
-      @sprites = [
-        Human.new(self),
-        Cat.new(self),
-        Bowl.new(self)
-      ]
+      self.human = Human.new(self)
+      self.bowl = Bowl.new(self)
+      self.cat = Cat.new(window: self,
+                         human: self.human,
+                         bowl: self.bowl)
+
+      @sprites = [cat, human, bowl]
     end
 
     def update
