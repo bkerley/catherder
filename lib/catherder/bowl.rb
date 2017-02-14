7437ec79d79c1787cd583ae808d97c44f6184f86
Bryce Kerley <bkerley@brycekerley.net>
2017-02-13 19:55:23 -0500

    starting positions for bowl and cat

--- lib/catherder/bowl.rb
+++ lib/catherder/bowl.rb
@@ -4,7 +4,10 @@ module Catherder
   class Bowl < Sprite
     def initialize(window)
       super window
-      load_image "human.png"
+      load_image "bowl.png"
+
+      self.x = window.width - self.image.width
+      self.y = window.height / 2
     end
   end
 end
--- lib/catherder/cat.rb
+++ lib/catherder/cat.rb
@@ -5,6 +5,9 @@ module Catherder
     def initialize(window)
       super window
       load_image "cat.png"
+
+      self.x = rand(window.width / 2) + (window.width / 4)
+      self.y = rand(window.height / 2) + (window.height / 4)
     end
   end
 end
