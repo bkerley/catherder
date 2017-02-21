1107ce1ef08e57f0c58e831ecc8bcaa53be2343e
Bryce Kerley <bkerley@brycekerley.net>
2017-02-20 19:55:31 -0500

    human speed limited when moving diagonally

--- lib/catherder/human.rb
+++ lib/catherder/human.rb
@@ -3,6 +3,7 @@ require 'catherder/sprite'
 module Catherder
   class Human < Sprite
     SPEED = 10
+    SQRT_TWO = Math.sqrt(2)
 
     def initialize(window)
       super window
@@ -16,18 +17,28 @@ module Catherder
       super
 
       adjusted_speed = SPEED * elapsed_frames
+      delta_x = 0
+      delta_y = 0
 
       if window.button_down? Gosu::KbLeft
-        self.x -= adjusted_speed
+        delta_x -= adjusted_speed
       elsif window.button_down? Gosu::KbRight
-        self.x += adjusted_speed
+        delta_x += adjusted_speed
       end
 
       if window.button_down? Gosu::KbUp
-        self.y -= adjusted_speed
+        delta_y -= adjusted_speed
       elsif window.button_down? Gosu::KbDown
-        self.y += adjusted_speed
+        delta_y += adjusted_speed
       end
+
+      if delta_x != 0 && delta_y != 0
+        delta_x /= SQRT_TWO
+        delta_y /= SQRT_TWO
+      end
+
+      self.x += delta_x
+      self.y += delta_y
     end
   end
 end
