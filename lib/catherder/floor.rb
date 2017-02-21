7c700483fb3d6a4554972e720fb017cdcc354184
Bryce Kerley <bkerley@brycekerley.net>
2017-02-20 19:51:18 -0500

    make floor scroll

--- lib/catherder/floor.rb
+++ lib/catherder/floor.rb
@@ -6,12 +6,15 @@ module Catherder
     end
 
     def draw
-      repeat_x = (window.width.to_f / image.width.to_f).ceil
+      scroll_amount = ((Gosu.milliseconds) / 10) % image.width
+
+      repeat_x = 3 + (window.width.to_f / image.width.to_f).ceil
       repeat_y = (window.height.to_f / image.height.to_f).ceil
 
       repeat_x.times do |col|
+        x_position = (image.width * (col)) - scroll_amount
         repeat_y.times do |row|
-          image.draw(image.width * col,
+          image.draw(x_position,
                      image.height * row,
                      0)
         end
